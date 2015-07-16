b46921233606467b24304a7f1b83f55c411843c3
Lia Skalkos <hello@liaskalkos.net>
2015-07-16 15:35:28 -0400

    Another refactor

--- js/simple-calc.js
+++ js/simple-calc.js
@@ -1,41 +1,35 @@
 $(function(){
 
   // write your solution here.
-    var $number_1 = $('#number1 .number');
-    var $number_2 = $('#number2 .number');
 
-    var number_1 = 0;
-    var number_2 = 0;
+    function numberButtons(element){
 
+        var counter = 0;
+
+        element.find('button').click(function() {
+
+            var type = $(this).attr('class');
+
+            if (type === 'incr') {
+                counter = counter + 1;
+            } else if (type === 'decr') {
+                counter = counter - 1;
+            };
+
+            element.find('.number').html(counter);
+        });
+
+        return {
+            getCounter: function(){
+                return counter;
+            }
+        }
+
+    }
+
+    var number_1_button = numberButtons($('#number1'));
+    var number_2_button = numberButtons($('#number2'));
     var operation;
-
-
-    $('#number1 button').click(function() {
-
-        var type = $(this).attr('class');
-
-        if (type === 'incr') {
-            number_1 = number_1 + 1;
-        } else if (type === 'decr') {
-            number_1 = number_1 - 1;
-        };
-
-        $number_1.html(number_1);
-    });
-
-
-    $('#number2 button').click(function() {
-
-        var type = $(this).attr('class');
-
-        if (type === 'incr') {
-            number_2 = number_2 + 1;
-        } else if (type === 'decr') {
-            number_2 = number_2 - 1;
-        };
-
-        $number_2.html(number_2);
-    });
 
     $( "div:eq( 1 ) button").click(function() {
         operation = $(this).html();
@@ -43,7 +37,8 @@ $(function(){
     });
 
     $('#equals').click(function() {
-        var result;
+        var number_1 = number_1_button.getCounter();
+        var number_2 = number_2_button.getCounter();
 
         switch(operation) {
             case '+':
@@ -65,5 +60,6 @@ $(function(){
         $('#result').html(result);
 
     })
+
 
 });
